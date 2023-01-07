6755bd137d97dcf7e73ecce2e6ae85471954b1f1
cardeoz <66281777+cardeoz@users.noreply.github.com>
2023-01-07 14:36:07 -0500

    ajusted del menu desktop y mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const navEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const burguerMenu=document.querySelector('.burguer-menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+
 
 navEmail.addEventListener('click',(toggleDesktopMenu));
+burguerMenu.addEventListener('click',(toggleMobileMenu));
 
 function toggleDesktopMenu()  {
     desktopMenu.classList.toggle('inactive');
 }
-console.log('funcionando')
+
+function toggleMobileMenu()  {
+    mobileMenu.classList.toggle('inactive');
+}
